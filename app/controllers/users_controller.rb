1adf81fd9e02af534acee52755bfb3d6f7ff046d
greenmoon55 <greenmoon55@gmail.com>
2012-11-18 20:35:22 +0800

    requiring the right user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -19,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -32,4 +34,14 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
