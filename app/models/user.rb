a88ae0555d8a5bf936cdabb984bd09af7e3d2af2
greenmoon55 <greenmoon55@gmail.com>
2012-12-05 23:24:38 +0800

    add follow! and unfollow! methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,22 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -25,6 +30,17 @@ class User < ActiveRecord::Base
     microposts
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
   private
     
     def create_remember_token
