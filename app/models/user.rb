141dc0ff0da2731b4e7940a3fba23ecd6519993d
greenmoon55 <greenmoon55@gmail.com>
2012-12-06 12:49:54 +0800

    finally finished!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,10 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by_followed_id(other_user.id).destroy
   end
+
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
   private
     
     def create_remember_token
